bf770d9c27ea2ef86dd5483a6a256dea7d006788
paulwongx <20178251+paulwongx@users.noreply.github.com>
2021-02-16 23:34:53 -0500

    initial commit

---
+++ webpack1/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+
+const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development'
+
+module.exports = {
+    mode: mode,
+
+    // entry: './src/index.js',
+    // output: {
+    //     filename: 'bundle.js',
+    //     path: path.resolve(__dirname, 'public')
+    // },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    // without additional settings, this will reference .babelrc
+                    loader: 'babel-loader'
+                }
+
+            }
+        ]
+    },
+
+    // devtool: 'source-map',
+
+    devServer: {
+        contentBase: './dist'
+    }
+}
